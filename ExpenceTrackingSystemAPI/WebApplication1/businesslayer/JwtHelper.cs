83ee526a8e117ac6202cef405defa12ef3af1eda
sawa <121893056+Sawachanamandakith@users.noreply.github.com>
2025-11-09 00:28:27 +0530

    Add files via upload

---
+++ ExpenceTrackingSystemAPI/WebApplication1/businesslayer/JwtHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebApplication1.BusinessLayer
+{
+    public class JwtTokenGenerator
+    {
+        private static readonly string SecretKey = "your_secret_key_heresdfdasdwaedsdasesa"; 
+
+        public static string GenerateToken(string email, string userIdentifier)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(SecretKey);
+
+            var tokenDescriptor = new Microsoft.IdentityModel.Tokens.SecurityTokenDescriptor 
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Email, email),
+                    new Claim("UserIdentifier", userIdentifier)
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new Microsoft.IdentityModel.Tokens.SigningCredentials( 
+                    new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+
+}
---
+++ ExpenceTrackingSystemAPI/WebApplication1/businesslayer/PasswordHasher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Helpers;
+
+
+namespace WebApplication1.BusinessLayer
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            try
+            {
+                return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+            }
+            catch
+            {
+                
+                return false;
+            }
+        }
+    }
+}
