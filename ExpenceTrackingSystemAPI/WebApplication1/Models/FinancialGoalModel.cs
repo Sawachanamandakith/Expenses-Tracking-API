9cc2c345bbfb3462e6226131b017da8e0664c12a
sawa <121893056+Sawachanamandakith@users.noreply.github.com>
2025-11-18 08:42:17 +0530

    add model files wishlist and goals

---
+++ ExpenceTrackingSystemAPI/WebApplication1/Models/FinancialGoalModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WebApplication1.Models
+{
+    public class FinancialGoalModel
+    {
+        public int GoalID { get; set; }
+        public int UserID { get; set; }
+        public string GoalName { get; set; }
+        public decimal TargetAmount { get; set; }
+        public decimal CurrentProgress { get; set; }
+        public string TargetDate { get; set; } 
+        public string Status { get; set; }
+        public string CreatedAt { get; set; }
+        public string UpdatedAt { get; set; }
+        public decimal ProgressPercent { get; set; }
+    }
+}
---
+++ ExpenceTrackingSystemAPI/WebApplication1/Models/WishListModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WebApplication1.Models
+{
+    public class WishListModel
+    {
+        public int WishID { get; set; }
+        public int UserID { get; set; }
+        public string ItemName { get; set; }
+        public decimal EstimatedCost { get; set; }
+        public string Priority { get; set; }
+        public string TargetDate { get; set; }
+        public string Status { get; set; }
+        public string CreatedAt { get; set; }
+        public string UpdatedAt { get; set; }
+    }
+}
