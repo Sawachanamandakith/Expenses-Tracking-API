9b7ce74422becb58e355fdfb42f07bab55de289c
sawa <121893056+Sawachanamandakith@users.noreply.github.com>
2025-11-18 08:40:06 +0530

    add a wishlist and goal files

---
+++ ExpenceTrackingSystemAPI/WebApplication1/Interfaces/IFinancialGoal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Models.RequestApiModels;
+
+namespace WebApplication1.Interfaces
+{
+    public interface IFinancialGoal
+    {
+        Response AddFinancialGoal(FinancialGoalRequestAPI request);
+        Response UpdateFinancialGoal(FinancialGoalRequestAPI request);
+        Response GetAllFinancialGoals(FinancialGoalRequestAPI request);
+    }
+}
---
+++ ExpenceTrackingSystemAPI/WebApplication1/Interfaces/IWishList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Models.RequestApiModels;
+
+namespace WebApplication1.Interfaces
+{
+    public interface IWishList
+    {
+        Response AddWish(WishListRequestAPI request);
+        Response MarkWishCompleted(WishListRequestAPI request);
+        Response GetAllWishes(WishListRequestAPI request);
+    }
+}
