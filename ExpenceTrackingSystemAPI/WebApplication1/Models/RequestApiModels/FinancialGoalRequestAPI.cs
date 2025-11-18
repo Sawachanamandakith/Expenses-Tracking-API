23c47adcf8946a7357faa3b3200d8f4bc22c4f69
sawa <121893056+Sawachanamandakith@users.noreply.github.com>
2025-11-18 08:43:43 +0530

    add request goal files

---
+++ ExpenceTrackingSystemAPI/WebApplication1/Models/RequestApiModels/FinancialGoalRequestAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WebApplication1.Models.RequestApiModels
+{
+    public class FinancialGoalRequestAPI : RequestAPI
+    {
+        public int? GoalID { get; set; }
+        public int? UserID { get; set; }
+        public string GoalName { get; set; }
+        public decimal? TargetAmount { get; set; }
+        public decimal? CurrentProgress { get; set; }
+        public DateTime? TargetDate { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ ExpenceTrackingSystemAPI/WebApplication1/Models/RequestApiModels/WishListRequestAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WebApplication1.Models.RequestApiModels
+{
+    public class WishListRequestAPI : RequestAPI
+    {
+        public int? WishID { get; set; }
+        public int? UserID { get; set; }
+        public string ItemName { get; set; }
+        public decimal? EstimatedCost { get; set; }
+        public string Priority { get; set; }
+        public string TargetDate { get; set; }
+        public string Status { get; set; }
+    }
+}
