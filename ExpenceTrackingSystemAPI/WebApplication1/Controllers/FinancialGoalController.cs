6b6d15cb17a40540165c76c934c05904db423a71
sawa <121893056+Sawachanamandakith@users.noreply.github.com>
2025-11-18 08:33:12 +0530

    add wishlist and goal controllers

---
+++ ExpenceTrackingSystemAPI/WebApplication1/Controllers/FinancialGoalController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using WebApplication1.Interfaces;
+using WebApplication1.Models.RequestApiModels;
+
+namespace WebApplication1.Controllers
+{
+    public class FinancialGoalController : Controller
+    {
+        private readonly IFinancialGoal _financialGoal;
+
+        public FinancialGoalController(IFinancialGoal financialGoal)
+        {
+            _financialGoal = financialGoal;
+        }
+
+        [HttpPost]
+        public ActionResult AddFinancialGoal(FinancialGoalRequestAPI request)
+        {
+            var result = _financialGoal.AddFinancialGoal(request);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateFinancialGoal(FinancialGoalRequestAPI request)
+        {
+            var result = _financialGoal.UpdateFinancialGoal(request);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GetAllFinancialGoals(int userId)
+        {
+            FinancialGoalRequestAPI request = new FinancialGoalRequestAPI
+            {
+                UserID = userId
+            };
+
+            var result = _financialGoal.GetAllFinancialGoals(request);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ExpenceTrackingSystemAPI/WebApplication1/Controllers/WishListController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using WebApplication1.Interfaces;
+using WebApplication1.Models.RequestApiModels;
+
+namespace WebApplication1.Controllers
+{
+    public class WishListController : Controller
+    {
+        private readonly IWishList _wishList;
+
+        public WishListController(IWishList wishList)
+        {
+            _wishList = wishList;
+        }
+
+        [HttpPost]
+        public ActionResult AddWish(WishListRequestAPI request)
+        {
+            var result = _wishList.AddWish(request);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult MarkWishCompleted(WishListRequestAPI request)
+        {
+            var result = _wishList.MarkWishCompleted(request);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GetAllWishes(int userId)
+        {
+            WishListRequestAPI request = new WishListRequestAPI { UserID = userId };
+            var result = _wishList.GetAllWishes(request);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
