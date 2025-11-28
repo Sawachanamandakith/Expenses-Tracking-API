fb1a7315326b6f6ae965f2fad3a29df5c293b45c
sawa <121893056+Sawachanamandakith@users.noreply.github.com>
2025-11-28 18:57:28 +0530

    Add files via upload

---
+++ ExpenceTrackingSystemAPI/WebApplication1/Interfaces/ITaskBudget.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Models.RequestApiModels;
+
+namespace WebApplication1.Interfaces
+{
+    public interface ITaskBudget
+    {
+        Response AddTask(TaskBudgetRequestAPI request);
+        Response UpdateTask(TaskBudgetRequestAPI request);
+        Response DeleteTask(TaskBudgetRequestAPI request);
+
+        Response GetAllTasks(TaskBudgetRequestAPI request);
+        Response GetTaskById(TaskBudgetRequestAPI request);
+
+        Response AddTaskItem(TaskBudgetRequestAPI request);
+        Response UpdateTaskItem(TaskBudgetRequestAPI request);
+        Response DeleteTaskItem(TaskBudgetRequestAPI request);
+
+        Response GetTaskItems(TaskBudgetRequestAPI request);
+        Response RecalculateTaskTotal(TaskBudgetRequestAPI request);
+    }
+}
