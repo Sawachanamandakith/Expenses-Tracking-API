acf1650f9738a2c149fff37b2aa0a879cedacd5f
sawa <121893056+Sawachanamandakith@users.noreply.github.com>
2025-11-28 18:56:44 +0530

    Add files via upload
    model files upload

---
+++ ExpenceTrackingSystemAPI/WebApplication1/Models/TaskBudgetItemModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WebApplication1.Models
+{
+    public class TaskBudgetItemModel
+    {
+        public int ItemID { get; set; }
+        public int TaskID { get; set; }
+        public string ItemName { get; set; }
+        public string Category { get; set; }
+        public decimal EstimatedCost { get; set; }
+        public string Note { get; set; }
+        public string Status { get; set; }
+        public string CreatedAt { get; set; }
+        public string UpdatedAt { get; set; }
+    }
+}
---
+++ ExpenceTrackingSystemAPI/WebApplication1/Models/TaskBudgetModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WebApplication1.Models
+{
+    public class TaskBudgetModel
+    {
+        public int TaskID { get; set; }
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public decimal MaxBudget { get; set; }
+        public decimal @EstimatedCost { get; set; }
+        public string Status { get; set; }
+        public string CreatedAt { get; set; }
+        public string UpdatedAt { get; set; }
+    }
+}
